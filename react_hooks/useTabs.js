ef79695c5589daf01c7e16dd965b922717ae51be
SiwooPak <bogus1620@naver.com>
2021-07-16 00:09:27 +0900

    modified: useTabs.js

--- react_hooks/useTabs.js
+++ react_hooks/useTabs.js
@@ -15,15 +15,26 @@ const content = [
 ];
 
 const useTabs = ((initTab, allTabs) => {
+    if(!allTabs || Array.isArray(allTabs)){
+        return;
+    }
     const [currentIndex, setCurrentIndex] = useState(initTab);
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    };
 })
 
 function App() {
-   
+   const { currentItem, changeItem} = useTabs(0,content);
     
     return (
       <div className="App">
         <h1>Hello </h1>
+        {content.map((section, idx) => {
+            <button  key={idx} onClick={()=>changeItem(idx)}>{section.tab}</button>
+        })}
+        <div>{currentItem.content}</div>
       </div>
     );
   }
